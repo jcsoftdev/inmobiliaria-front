955309c700bc987f6096a1e4feb07743e9e36e20
Juan Carlos <ajcarlos032@gmail.com>
2025-02-06 17:17:22 -0500

    feat: add integration of properties api

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ function App() {
     <div className="p-4 ">
       <h1 className="text-3xl ">Comenzando el proyecto de la inmobiliaria</h1>
       <p>Lorem ipsum dolor sit amet.</p>
+
+      <p>Estoy integrando la api de properties</p>
     </div>
   )
 }
