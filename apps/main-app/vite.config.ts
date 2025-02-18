6907259aece56dcf26f8d7cdc306e99633f7310a
Juan Carlos <ajcarlos032@gmail.com>
2025-02-18 16:11:48 -0500

    feat: restructure app to utilize HeroUI and downgrade tailwind version

--- apps/main-app/vite.config.ts
+++ apps/main-app/vite.config.ts
@@ -1,17 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path';
-
+// TODO: upgrade to v4 when heroui is released
+// import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@libs/ui': path.resolve(__dirname, '../../packages/ui/src'),
-    }
+      '@libs/utils': path.resolve(__dirname, '../../packages/utils/src'),
+    },
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    // TODO: upgrade to v4 when heroui is released
+    // tailwindcss()
+  ],
   build: {
     sourcemap: true,
     target: 'esnext',
