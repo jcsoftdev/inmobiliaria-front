64023bd0732bddc1d2d99ffa96ef91c3b1dae3e1
Juan Carlos <ajcarlos032@gmail.com>
2025-02-18 10:34:20 -0500

    chore: change to monorepo app

--- apps/main-app/vite.config.ts
+++ apps/main-app/vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
+
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@libs/ui': path.resolve(__dirname, '../../packages/ui/src'),
+    }
+  },
   plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: true,
+    target: 'esnext',
+    outDir: '../../dist',
+  },
 })
