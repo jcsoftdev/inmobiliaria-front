83da869d05236b110076db288a4e80ba1600d479
Juan Carlos <ajcarlos032@gmail.com>
2025-04-03 15:59:24 -0500

    feat: update deletion handling in companies store to manage loading state

--- src/store/companies.store.ts
+++ src/store/companies.store.ts
@@ -98,6 +98,8 @@ export const createSetters = (
       (prev) => ({
         ...prev,
         data: prev.data?.filter((company) => company.id !== id),
+        isLoading:
+          prev.data?.filter((company) => company.id !== id).length === 0,
       }),
       false,
       'company/deleteFromCompanies',
